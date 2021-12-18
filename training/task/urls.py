60d6693fcf2966485fe27a2f6b6ba850c6b86d52
D SAI YESHWANTH <60391044+saiyeshwanth28@users.noreply.github.com>
2021-12-18 17:34:38 +0530

    Update urls.py

--- training/task/urls.py
+++ training/task/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.store,name="store"),
     path('register/',views.registerPage,name="register"),
     path('login/',views.loginPage,name="login"),
+    path('search/',views.search,name="search"),
     path('cart/',views.cart,name="cart"),
     path('checkout/', views.checkout, name="checkout"),
 ]
